8d9b4d33fa9711f6f96d442dc5e8da006182fcad
Francisco Castillo <149974617+FranJCastilloC@users.noreply.github.com>
2024-03-19 20:05:56 -0400

    1

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ from modelo import modulo_modelo
 # Configura la página para utilizar todo el ancho disponible
 st.set_page_config(layout="wide")
 
-def load_css(file_name):
-    import os
-    base_path = os.path.dirname(__file__)  # Obtiene la ruta del directorio donde se encuentra el script actual
-    file_path = os.path.join(base_path, file_name)  # Construye la ruta completa usando la ruta relativa
-    with open(file_path) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-# Aquí cambiamos la ruta a una relativa. Asegúrate de ajustar 'assets/style.css' a tu estructura de directorios.
-load_css('C:/Users/franc/OneDrive - INTEC/Escritorio/Proyecto ARS/Dashboards/Assets/style.css')
 
 # Creación del menú lateral para la navegación
 st.sidebar.title("Menú")
